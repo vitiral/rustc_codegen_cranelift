d987412990f0ddb8d51853c6ae7b4ff2c74e37d7
bjorn3 <bjorn3@users.noreply.github.com>
2019-08-31 18:40:52 +0200

    Improve test filter

--- test.sh
+++ test.sh
@@ -67,18 +67,14 @@ rustc = "$HOME/.rustup/toolchains/nightly-$TARGET_TRIPLE/bin/rustc"
 EOF
 
 #rm -r src/test/ui/{asm-*,abi*,extern/,panic-runtime/,panics/,unsized-locals/,proc-macro/,threads-sendsync/,thinlto/,simd*,borrowck/,test*,*lto*.rs} || true
-#for test in $(rg --files-with-matches "ignore-emscripten|ignore-wasm32|asm!" src/test/ui); do
-#   rm $test
-#done
-#rm src/test/ui/confuse-field-and-method/issue-2392.rs || true # Borrowck error difference
-#rm src/test/ui/span/borrowck-call-is-borrow-issue-12224.rs || true # ^
+for test in $(rg --files-with-matches "asm!|catch_unwind|should_panic|thread" src/test/ui); do
+  rm $test
+done
 #rm src/test/ui/consts/const-size_of-cycle.rs || true # Error file path difference
 #rm src/test/ui/impl-trait/impl-generic-mismatch.rs || true # ^
 #rm src/test/ui/type_length_limit.rs || true
 #rm src/test/ui/huge-{array,array-simple-64,enum,struct}.rs || true # cg_clif doesn't provide a span for type is too big for arch
 #rm src/test/ui/issues/issue-15919-64.rs || true # ^
-#rm src/test/ui/issues/issue-23458.rs || true # Inline asm
-#rm src/test/ui/lint/lint-ctypes-enum.rs || true # NonZeroU128 patched away
 #rm src/test/ui/issues/issue-50993.rs || true # Target `thumbv7em-none-eabihf` is not supported
 #rm src/test/ui/linkage-attr/linkage3.rs || true # Different error
 #rm src/test/ui/macros/same-sequence-span.rs || true # Proc macro .rustc section not found?
